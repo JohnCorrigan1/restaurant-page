c3c485366e363b9d87f6c545d7c2fae7ac1fcd03
John C <jcorrigan322@gmail.com>
2022-08-28 10:26:42 -0400

    menu items populating

--- src/pages/menuPage.js
+++ src/pages/menuPage.js
@@ -1,6 +1,9 @@
+import { first } from "lodash";
+
 export default function loadMenu(){
 
     const content = document.querySelector('.content')
+
     const main = document.createElement('div')
     main.setAttribute('id', 'main')
     content.appendChild(main);
@@ -9,4 +12,27 @@ export default function loadMenu(){
     menuTitle.classList.add('menu-title')
     menuTitle.textContent = "Our Menu"
     main.appendChild(menuTitle)
+
+    const gridContainer = document.createElement('div')
+    gridContainer.classList.add('grid-container')
+    main.appendChild(gridContainer)
+
+    for(let i = 0; i < 10; i++){
+    gridContainer.appendChild(menuItem())
+    }
+    // for(i = 0; i < 10; i++){
+    //     const gridItem = document.createElement('div')
+    //     gridItem.classList.add('grid-item')
+    //     gridItem.textContent = "This is a test pls work"
+    //     gridContainer.appendChild('grid-item')
+    // }
+    // gridContainer.appendChild(menuItem())
+    }
+
+
+    function menuItem(){
+        const gridItem = document.createElement('div')
+        gridItem.classList.add('menu-item')
+        gridItem.textContent = "This is a test"
+        return gridItem
     }
