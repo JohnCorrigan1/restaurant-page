25b9bc22ed901da970d24aa17cde9ce4df48be02
John C <jcorrigan322@gmail.com>
2022-08-29 14:23:25 -0400

    contact page done

--- src/pages/contactPage.js
+++ src/pages/contactPage.js
@@ -1,3 +1,5 @@
+import Giza from './../imgs/giza.png'
+
 export default function loadContact(){
 
     const content = document.querySelector('.content')
@@ -5,9 +7,20 @@ export default function loadContact(){
     main.setAttribute('id', 'main')
     content.appendChild(main);
     
-    const contactTitle = document.createElement('div')
-    contactTitle.classList.add('contact-title')
-    contactTitle.textContent = "Contact us here"
-    main.appendChild(contactTitle)
-    
-    }
+    const contactDiv = document.createElement('div')
+    contactDiv.classList.add('contact-div')
+    main.appendChild(contactDiv)
+
+    const phoneNum = document.createElement('p')
+    phoneNum.textContent = "Schedule a reservation or place an order for pickup at: 100-200-3000"
+    contactDiv.appendChild(phoneNum)
+
+    const address = document.createElement('p')
+    address.textContent = "Nazlet El-Semman, Al Haram, Giza Governorate 3512201, Egypt"
+    contactDiv.appendChild(address)
+
+    const giza = new Image();
+    giza.src = Giza;
+    giza.classList.add('giza')
+    contactDiv.appendChild(giza)
+}
--- src/pages/homePage.js
+++ src/pages/homePage.js
@@ -17,9 +17,7 @@ about.classList.add('about')
 about.textContent = "This image of the chef was created by DALL.E an AI that generates images from text prompts. That's why it looks so weird. The prompt for this image was 'professional chef happily making a large pizza in a fancy restaurant studio lighting'"
 aboutDiv.appendChild(about)
 
-// const chef = document.createElement('img')
 const chef = new Image();
-// chef.src = './imgs/pizza_chef.jpg'
 chef.src = Chef;
 chef.classList.add('chef')
 aboutDiv.appendChild(chef)
